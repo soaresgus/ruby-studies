27bf07d6d1a325832d4e84132178288fb675acef
soaresgus <gustavosoaresbt@hotmail.com>
2023-07-05 10:40:00 -0300

    operadores e tipos de dados

---
+++ operadores/operadores.rb
@@ -0,0 +1,9 @@
+var1 = 10
+var2 = 2
+
+puts var1 + var2
+puts var1 - var2
+puts var1 * var2
+puts var1 / var2
+puts var1 % var2
+puts var1 ** var2
---
+++ tipos-dados/tipos-dados.rb
@@ -0,0 +1,30 @@
+int = 10
+puts int
+puts int.class
+
+float = 5.5
+puts float
+puts float.class
+
+bootrue = true
+boofalse = false
+puts bootrue
+puts bootrue.class
+puts boofalse
+puts boofalse.class
+
+string = "hello world"
+puts string
+puts string.class
+
+array = ['a', 'b', 'c', 'd', 'e', 'f']
+puts array
+puts array.class
+
+symbol = :teste
+puts symbol
+puts symbol.class
+
+hash = {name: 'gustavo', language: 'pt-br'}
+puts hash
+puts hash[:name]
